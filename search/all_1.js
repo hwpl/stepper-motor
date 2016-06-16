cc4fc8aaf3f97b8ce69762129d30aedff5cd7a33
Klemens Schölhorn <klemens@schoelhorn.eu>
2016-06-16 19:19:17 +0200

    Update documentation

--- search/all_1.js
+++ search/all_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['stepper_2dmotor',['stepper-motor',['../index.html',1,'']]],
-  ['setstepsperrotation',['setStepsPerRotation',['../classStepperMotor_1_1Driver.html#a6446035c8db46ba4daa04ea890545dc8',1,'StepperMotor::Driver']]]
+  ['setstepsperrotation',['setStepsPerRotation',['../classStepperMotor_1_1Driver.html#a6446035c8db46ba4daa04ea890545dc8',1,'StepperMotor::Driver']]],
+  ['setwaiter',['setWaiter',['../classStepperMotor_1_1Driver.html#a1bdfe3c00eeadc598401b42f68ba0f4c',1,'StepperMotor::Driver']]]
 ];
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['setstepsperrotation',['setStepsPerRotation',['../classStepperMotor_1_1Driver.html#a6446035c8db46ba4daa04ea890545dc8',1,'StepperMotor::Driver']]]
+  ['setstepsperrotation',['setStepsPerRotation',['../classStepperMotor_1_1Driver.html#a6446035c8db46ba4daa04ea890545dc8',1,'StepperMotor::Driver']]],
+  ['setwaiter',['setWaiter',['../classStepperMotor_1_1Driver.html#a1bdfe3c00eeadc598401b42f68ba0f4c',1,'StepperMotor::Driver']]]
 ];
